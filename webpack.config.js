996d8ec9e1cb489ee9f32f13728cd10de6f54eea
Thanh Hai Dev <hai2571998@gmail.com>
2018-12-01 23:19:18 +0700

    Init React App for Sales Management Website

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js"
+    },
+    module: {
+        rules: [{
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    mode: "development",
+    plugins: [htmlPlugin]
+}
